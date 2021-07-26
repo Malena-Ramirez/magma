d087796e19f089ab69c4dbbb16b1bc5a43078fac
geriosla99 <geriosla@gmail.com>
2021-07-26 18:11:48 -0500

    create styles for signup and some functions

--- src/components/Signup/SignUp.jsx
+++ src/components/Signup/SignUp.jsx
@@ -1,10 +1,119 @@
 import React from 'react'
+import {
+    ArrowBack,
+    LoginContainer,
+    ProfileIcon,
+    AltLoginContainer,
+    AltIconContainer,
+    AlternativeText,
+    AlternativeTextLink
+  } from '../Join/Join';
+import { Form, Button, Container, FloatingLabel } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loginGoogle } from '../../action/action';
 
 const SignUp = () => {
+
+    const dispatch = useDispatch();
+
+    const history = useHistory();
+    const handleReturn = (e) => {
+      history.goBack();
+    };
+
+    const handleJoin = () => {
+        history.push("/Join");
+      }
+
+      const handleLoginGoogle = () => {
+        dispatch(loginGoogle());
+      };
+
     return (
-        <div>
-            Soy SignUp
+        <>
+      <div className='mt-3 ms-3'>
+        <ArrowBack
+          className='bi bi-arrow-left-circle'
+          onClick={handleReturn}
+        ></ArrowBack>
+      </div>
+      <LoginContainer>
+        <h1 className='text-center'>Registrarse</h1>
+        <div className='d-flex justify-content-center my-1'>
+          <ProfileIcon className='bi bi-person-circle'></ProfileIcon>
         </div>
+        <Form>
+        <Form.Group className='mb-3' controlId='formName'>
+            <FloatingLabel controlId='floatingName' label='Nombres y Apellidos'>
+              <Form.Control
+                type='text'
+                placeholder='Nombres y Apellidos'
+                name='name'
+                required
+                // value={email}
+                // onChange={handleInputChange}
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <FloatingLabel controlId='floatingEmail' label='Correo electrónico'>
+              <Form.Control
+                type='email'
+                placeholder='Correo electrónico'
+                name='email'
+                required
+                // value={email}
+                // onChange={handleInputChange}
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formBasicPassword'>
+            <FloatingLabel controlId='floatingPassword' label='Contraseña'>
+              <Form.Control
+                type='password'
+                placeholder='Contraseña'
+                name='password'
+                required
+                // value={password}
+                // onChange={handleInputChange}
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formBasicPassword'>
+            <FloatingLabel controlId='floatingPassword2' label='Confirmar Contraseña'>
+              <Form.Control
+                type='password2'
+                placeholder='Confirmar Contraseña'
+                name='password2'
+                required
+                // value={password}
+                // onChange={handleInputChange}
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <div className='d-flex justify-content-center my-3'>
+            <Button size='lg' variant='warning' type='submit'>
+              Registrarse
+            </Button>
+          </div>
+          <AlternativeText>
+            ¿Ya tienes una cuenta? <AlternativeTextLink onClick={handleJoin}>Inicia seción</AlternativeTextLink>
+          </AlternativeText>
+          <Container>
+            <p className='text-center'>O ingresa con</p>
+            <AltLoginContainer>
+              <AltIconContainer>
+                <i onClick={handleLoginGoogle} className='bi bi-google'></i>
+              </AltIconContainer>
+              <AltIconContainer>
+                <i className='bi bi-facebook'></i>
+              </AltIconContainer>
+            </AltLoginContainer>
+          </Container>
+        </Form>
+      </LoginContainer>
+    </>
     )
 }
 
