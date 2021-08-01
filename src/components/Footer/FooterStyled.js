c193cf8056d4147b6e75d883dab05c98479cb863
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-01 17:33:00 -0500

    Style footer

--- src/components/Footer/FooterStyled.js
+++ src/components/Footer/FooterStyled.js
@@ -2,25 +2,25 @@ import styled from "styled-components"
 
 export const FooterContent = styled.div`
     background: #0F182E;
-    border-top: 5px solid #FCA311;
+    border-top: 35px solid #FCA311;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 20px;
+    padding: 25px;
     position: relative;
 `;
 
 export const LogoImg = styled.img`
-    width: 60px;
+    width: 85px;
+    margin-bottom: 25px;
     @media (min-width: 768px){
-        width: 80px;
-}
+        width: 110px;
+    }
 `;
 
 export const ContentLogo = styled.div`
     display: flex;
-
 `;
 
 export const IconsContainer = styled.div`
@@ -28,10 +28,21 @@ export const IconsContainer = styled.div`
     justify-content: center;
     align-items: center;
     width:100%;
-    color: #ccc; 
+    color: #EBEBEB; 
 `;
 
 export const RRSS = styled.i`
     margin: 0.5rem;
     display: flex;
+    font-size: 1.2rem;
+    cursor: pointer;
+    &:hover{
+        color: #FCA311;
+    }
 `;
+
+export const AboutUs = styled.p`
+    color: #EBEBEB; 
+    margin: 0;
+    margin-top: 10px;
+`
