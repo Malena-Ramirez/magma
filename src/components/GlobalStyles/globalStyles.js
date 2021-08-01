ec3e740839360b894a87d7758e87c211f0bc7d4b
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-01 18:25:18 -0500

    Add global styles

--- src/components/GlobalStyles/globalStyles.js
+++ src/components/GlobalStyles/globalStyles.js
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
+import { color } from "../GlobalStyles/color";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     font-family: 'Cabin', sans-serif;
+    color: ${color.dark};
+    background-color: ${color.light}
   }
 `;
 
