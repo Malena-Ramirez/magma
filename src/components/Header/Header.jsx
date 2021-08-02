1c4677b9bb106f99d2824f9f32f37cb649a901f0
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-02 18:11:16 -0500

    Style private header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,45 +17,47 @@ const Header = () => {
     <Navbar
       sticky='top'
       variant='dark'
-      expand='lg'
+      expand='md'
       id='costum-navbar-container'
     >
-      <Container>
+      <Container fluid>
         <Navbar.Brand href='/'>
           <img
+            id='navbar-logo'
             src='https://i.imgur.com/8saUX2R.png'
-            width='90'
             className='d-inline-block align-top'
             alt='Logo de Magma'
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mx-auto'>
+          <Nav className='mx-auto' id='items-navbar'>
             <Nav.Link href='/'>Inicio</Nav.Link>
             <Nav.Link href='/empleos'>Empleos</Nav.Link>
             <Nav.Link href='/capacitaciones'>Capacitaciones</Nav.Link>
             <Nav.Link href='/contacto'>Contáctanos</Nav.Link>
           </Nav>
-          <Form className='d-flex'>
+          <Form className='d-flex me-3'>
             <FormControl
               type='search'
               placeholder='Buscar'
               className='mr-2'
               aria-label='Search'
             />
-            <Button variant='outline-warning'>Search</Button>
-            <Nav>
-              <Dropdown as={NavItem}>
-                <Dropdown.Toggle as={NavLink}>
-                  <i className='bi bi-person-circle'></i>
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item>Hello there!</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav>
+            <Button variant='outline-warning'>Buscar</Button>
           </Form>
+          <Nav>
+            <Dropdown drop='start' as={NavItem}>
+              <Dropdown.Toggle className='text-warning' as={NavLink}>
+                <i className='bi bi-person-circle text-warning fs-4'></i>
+              </Dropdown.Toggle>
+              <Dropdown.Menu className='bg-warning'>
+                <Dropdown.Item>Mensajes</Dropdown.Item>
+                <Dropdown.Item>Editar perfil</Dropdown.Item>
+                <Dropdown.Item>Cerrar sesión</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
