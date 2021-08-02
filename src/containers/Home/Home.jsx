2c315a35105d3c4e9f758c958bb24ab3e4a2eee9
geriosla99 <geriosla@gmail.com>
2021-08-02 16:34:26 -0500

    make styles for home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { ContainerTopHome, ContainerJobandLessons, ImageCard, TitleText, TextHome, ContainerAll } from "./HomeStyled";
 
 const Home = () => {
   return (
-    <div style={{ marginTop: '500px' }}>
-      <Link to='/contact'>Contacto</Link>
-      <h1>Soy el home</h1>
-    </div>
+    <>
+      <ContainerTopHome>
+      </ContainerTopHome>
+      <ContainerAll >
+
+        <ContainerJobandLessons>
+          <ImageCard src="https://i.imgur.com/ef8jGHF.jpg" alt="Buscar empleo"/>
+          <span>
+            <TitleText>Empleos</TitleText>
+            <TextHome>Descubre nuestras ofertas de empleo</TextHome>
+          </span>          
+        </ContainerJobandLessons>
+
+        <ContainerJobandLessons>
+          <ImageCard src="https://i.imgur.com/UF5sES4.jpg" alt="Buscar capacitación"/>
+          <span>            
+            <TitleText>Capacitaciones</TitleText>
+            <TextHome>Nunca dejes de aprender akenkrnvr</TextHome>
+          </span>        
+        </ContainerJobandLessons>
+
+      </ContainerAll>
+    </>
   );
 };
 
---
+++ src/containers/Home/HomeStyled.js
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+
+export const ContainerTopHome = styled.div`
+    background-image: url('https://i.imgur.com/J99xLst.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100%;
+    height: 30vh;
+    filter: brightness(40%); // Más oscura
+    margin-bottom: 2em;
+`;
+
+export const ContainerAll= styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 15px;
+    padding: 20px;
+    @media (min-width: 700px){
+        grid-template-columns: 1fr 1fr;
+    }
+`;
+
+export const ContainerJobandLessons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px; 
+    margin-bottom: 2em;  
+    box-shadow: 0px 0px 13px -1px rgba(0,0,0,0.38);
+    -webkit-box-shadow: 0px 0px 13px -1px rgba(0,0,0,0.38);
+    -moz-box-shadow: 0px 0px 13px -1px rgba(0,0,0,0.38); 
+    border-radius: 0.2rem;
+    
+`;
+
+export const ImageCard = styled.img`
+    width: 20vw;
+    margin: 0.9em;
+    border-radius: 0.2rem;
+`;
+
+export const TextHome = styled.p`
+    font-size: 0.3;
+`;
+
+export const TitleText = styled.h3`
+    display: flex;
+    font-size: 1.2em;
+`;
+
+export const ContainerText = styled.span`
+    display: flex;
+    flex-direction: column;
+`;
