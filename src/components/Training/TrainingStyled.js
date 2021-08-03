a8b268ac67de6750d0f0f79bf0128ff86056e04e
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-02 22:58:50 -0500

    Render training cards

--- src/components/Training/TrainingStyled.js
+++ src/components/Training/TrainingStyled.js
@@ -17,3 +17,29 @@ export const TrainingTitle = styled.h1`
   color: ${color.main};
   font-weight: 800;
 `;
+
+export const TrainingContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: 20px;
+  @media(min-width: 768px){
+    grid-template-columns: 1fr 3fr;
+    padding: 20px 30px;
+    gap: 5px;
+  }
+  @media(min-width: 992px){
+    gap: 25px;
+    padding: 20px 50px;
+  }
+`;
+
+export const AsideBar = styled.aside`
+padding-top: 10px;
+  @media(min-width: 992px){
+    padding: 10px;
+  }
+`;
+
+export const MainContent = styled.main`
+  padding: 10px;
+`;
