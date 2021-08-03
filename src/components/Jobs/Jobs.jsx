84b6c59a443760e50410493481fedf35127b8847
geriosla99 <geriosla@gmail.com>
2021-08-02 20:06:43 -0500

    add more jobs

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -44,6 +44,20 @@ const Jobs = () => {
                     <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
                     </ContainerText>
                 </ChooseTheJob>
+                <ChooseTheJob>
+                    <LogoBusiness src="https://i.imgur.com/SY2GsO0.png" alt="LogoIndustries"/>
+                    <ContainerText>                        
+                    <BusinessName>Logo Industries</BusinessName>
+                    <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
+                    </ContainerText>
+                </ChooseTheJob>
+                <ChooseTheJob>
+                    <LogoBusiness src="https://i.imgur.com/zL3I65j.jpg" alt="Textiles"/>
+                    <ContainerText>                        
+                    <BusinessName>Aritex</BusinessName>
+                    <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
+                    </ContainerText>
+                </ChooseTheJob>
             </ContainerInfoJobs>
         </>
     )
