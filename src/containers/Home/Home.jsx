6a04bd45f34dbee89fdcaefdf101164b4a989dab
geriosla99 <geriosla@gmail.com>
2021-08-03 00:09:37 -0500

    change routes in the div of jobs and training

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { ContainerTopHome, ContainerJobandLessons, ImageCard, TitleText, TextHome, ContainerAll } from "./HomeStyled";
+import { useHistory } from "react-router-dom";
+
 
 const Home = () => {
+
+  const history = useHistory();
+  const handleClick = () =>{
+    console.log("hola desde capacitaciones");
+    history.push("/capacitaciones");
+  }
+
+  const handleClickJobs = () =>{
+    console.log("hola desde empleos");
+    history.push("/empleos");
+  }
+
   return (
     <>
       <ContainerTopHome>
       </ContainerTopHome>
       <ContainerAll >
 
-        <ContainerJobandLessons>
+
+        <ContainerJobandLessons onClick={handleClickJobs}>
           <ImageCard src="https://i.imgur.com/ef8jGHF.jpg" alt="Buscar empleo"/>
           <span>
             <TitleText>Empleos</TitleText>
@@ -16,7 +31,7 @@ const Home = () => {
           </span>          
         </ContainerJobandLessons>
 
-        <ContainerJobandLessons>
+        <ContainerJobandLessons onClick={handleClick}>
           <ImageCard src="https://i.imgur.com/UF5sES4.jpg" alt="Buscar capacitación"/>
           <span>            
             <TitleText>Capacitaciones</TitleText>
