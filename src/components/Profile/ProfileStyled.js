873811f5a34817ffc75fa3a5100518796061bd97
geriosla99 <geriosla@gmail.com>
2021-08-03 00:27:13 -0500

    add styles

--- src/components/Profile/ProfileStyled.js
+++ src/components/Profile/ProfileStyled.js
@@ -1,7 +1,10 @@
-
+import styled from "styled-components"
 
 export const ContainerNamePhoto = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 2em;
 `;
 
 export const PhotoProfile = styled.img`
