88fcc3fbf1e8db91050a10a8c76d0b08c594acec
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-05 16:24:31 -0500

    Style training

--- src/components/Training/TrainingDetail/TrainingDetailStyled.js
+++ src/components/Training/TrainingDetail/TrainingDetailStyled.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
+import { color } from "../../GlobalStyles/color";
 
 export const TrainingDetailContainer = styled.div`
   padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TrainingDetailTitle = styled.h1`
+  text-align: center;
+  color: ${color.main};
+  font-weight: 800;
+  margin: 20px 0;
+`;
+
+export const DescriptionContainer = styled.p`
+  text-align: justify;
+  width: 850px;
+  margin-bottom: 25px; 
 `;
