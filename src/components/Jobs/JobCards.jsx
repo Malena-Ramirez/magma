e973011311d4905fcfb30656edb6d86c2e565f0c
geriosla99 <geriosla@gmail.com>
2021-08-05 01:10:02 -0500

    put the cards of jobs here

--- src/components/Jobs/JobCards.jsx
+++ src/components/Jobs/JobCards.jsx
@@ -4,23 +4,27 @@ import {
     LogoBusiness, 
     ContainerText, 
     BusinessName, 
-    BusinessDescription } from "./JobStyled";
-const JobCards = () => {
-    const jobCard = [
-        "https://i.imgur.com/NQDgHj8.png",
-        "https://i.imgur.com/SY2GsO0.png",
-        "https://i.imgur.com/zL3I65j.jpg"
-    ]
+    BusinessDescription } from "./JobsStyled";
+import MyVerticallyCenteredModal from "./ModalJobs";
+
+const JobCards = ({image, name}) => {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
-        <div>
-            <ChooseTheJob>
-                        <LogoBusiness src="https://i.imgur.com/zL3I65j.jpg" alt="Textiles"/>
-                        <ContainerText>                        
-                        <BusinessName>Aritex</BusinessName>
-                        <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
-                        </ContainerText>
-                    </ChooseTheJob>
-        </div>
+        <>
+            <ChooseTheJob onClick={() => setModalShow(true)}>
+                <LogoBusiness src={image} alt={name}/>
+                <ContainerText>                        
+                    <BusinessName>{name}</BusinessName>
+                    <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
+                </ContainerText>
+            </ChooseTheJob>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </>
     )
 }
 
