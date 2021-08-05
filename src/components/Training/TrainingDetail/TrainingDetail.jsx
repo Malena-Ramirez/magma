82987be2251d93fbb19d82ce888a6d29328bf254
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-04 22:12:34 -0500

    Create training detail component

---
+++ src/components/Training/TrainingDetail/TrainingDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrainingDetail = () => {
+  return (
+    <div>
+      <h1>Detalle de capacitaciones</h1>
+    </div>
+  );
+};
+
+export default TrainingDetail;
