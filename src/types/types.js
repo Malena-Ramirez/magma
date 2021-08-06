0c21f9a6430746572ce56fff571bef11af243168
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-06 18:18:14 -0500

    Add types for training card

--- src/types/types.js
+++ src/types/types.js
@@ -1,4 +1,12 @@
 export const types = {
-    login : 'login',
-    logout: 'logout'
+    login: 'login',
+    logout: 'logout',
+
+    trainingCardNew: '[Training] new training',
+    trainingCardActive: '[Training] Set training card active',
+    trainingCardLoad: '[Training] Load training card',
+    trainingCardUpdate: '[Training] Update training card',
+    trainingCardDelete: '[Training] Delete training card',
+    trainingCardLogoutClean: '[Training] Logout',
+
 }
