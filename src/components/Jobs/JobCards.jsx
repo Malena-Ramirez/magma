6ad549af5b63c909222050b692eca59172a69254
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-05 22:53:56 -0500

    Add onClick function to button in modal

--- src/components/Jobs/JobCards.jsx
+++ src/components/Jobs/JobCards.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import { 
-    ChooseTheJob, 
-    LogoBusiness, 
-    ContainerText, 
-    BusinessName, 
-    BusinessDescription } from "./JobsStyled";
-import MyVerticallyCenteredModal from "./ModalJobs";
+import React from 'react';
+import {
+  ChooseTheJob,
+  LogoBusiness,
+  ContainerText,
+  BusinessName,
+  BusinessDescription,
+} from './JobsStyled';
+import MyVerticallyCenteredModal from './ModalJobs';
 
-const JobCards = ({image, name}) => {
+const JobCards = ({ image, name }) => {
+  const [modalShow, setModalShow] = React.useState(false);
 
-    const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <>
+      <ChooseTheJob onClick={() => setModalShow(true)}>
+        <LogoBusiness src={image} alt={name} />
+        <ContainerText>
+          <BusinessName>{name}</BusinessName>
+          <BusinessDescription>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere
+            eu sapien vitae viverra. Phasellus justo mauris, pharetra a
+            venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In
+            egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam
+            sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero
+            non ex semper, a sagittis sapien malesuada.
+          </BusinessDescription>
+        </ContainerText>
+      </ChooseTheJob>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+};
 
-    return (
-        <>
-            <ChooseTheJob onClick={() => setModalShow(true)}>
-                <LogoBusiness src={image} alt={name}/>
-                <ContainerText>                        
-                    <BusinessName>{name}</BusinessName>
-                    <BusinessDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In posuere eu sapien vitae viverra. Phasellus justo mauris, pharetra a venenatis vitae, ultricies vulputate tellus. Suspendisse potenti. In egestas tortor eget dui commodo vehicula. Nulla facilisi. Nullam sollicitudin leo nec urna sagittis volutpat. Duis hendrerit libero non ex semper, a sagittis sapien malesuada.</BusinessDescription>
-                </ContainerText>
-            </ChooseTheJob>
-            <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-        </>
-    )
-}
-
-export default JobCards
+export default JobCards;
--- src/components/Jobs/ModalJobs.jsx
+++ src/components/Jobs/ModalJobs.jsx
@@ -1,49 +1,51 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 const MyVerticallyCenteredModal = (props) => {
-
-  const handleClick = () =>{
-    console.log('si se envia');
+  const handleClick = () => {
+    props.onHide();
     Swal.fire({
       title: 'Exitos!',
       text: 'Tu Hoja de vida ha sido enviada',
       icon: 'success',
-      confirmButtonText: 'me encanta'
-    })
-  }
+      confirmButtonText: 'me encanta',
+    });
+  };
 
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <h4 className='align-middle text-center'>Nombre de la empresa</h4>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='row'>
-        <img src='https://i.imgur.com/jFJEnfb.png' 
-              alt='Nombre de la empresa' 
-              style={{width: 100}}
-              className='col-3'/>
-          <p className='col-9'>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+  return (
+    <Modal
+      {...props}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          <h4 className='align-middle text-center'>Nombre de la empresa</h4>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='row'>
+        <img
+          src='https://i.imgur.com/jFJEnfb.png'
+          alt='Nombre de la empresa'
+          style={{ width: 100 }}
+          className='col-3'
+        />
+        <p className='col-9'>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleClick} variant='outline-warning'>
+          Postular
+        </Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleClick} variant="outline-warning">Postular</Button>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-  
 export default MyVerticallyCenteredModal;
