b0bac6b5c273168af7850e5b18041e4bd5bdd1ec
geriosla99 <geriosla@gmail.com>
2021-08-07 00:57:32 -0500

    make a reducer for jobs

---
+++ src/reducers/jobsReducer.js
@@ -0,0 +1,18 @@
+import { types } from "../types/types";
+
+const initialState = {
+    jobs: []
+}
+
+export const jobsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.jobsAddNew:
+            return{
+                ...action.state,
+                jobs: [action.payload, ...state.jobs]
+            }
+    
+        default:
+            return state;
+    }
+}
