b4b24b5fd8296740cd4bb7bd674689cf5cb79cb1
geriosla99 <geriosla@gmail.com>
2021-08-07 00:58:03 -0500

    add more styles to component profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,22 +1,68 @@
 import React from 'react'
-import { ContainerNamePhoto } from "./ProfileStyled";
-import { Image } from 'react-bootstrap'
+import { ContainerNamePhoto, UserName, UserInfo, UserBasicInfo, AboutMeText, AboutMe, TitleAboutMe } from "./ProfileStyled";
+import { Image, Container } from 'react-bootstrap'
 
 const Profile = () => {
     return (
         <>
-            <ContainerNamePhoto>
-                <Image 
-                    style={{width:"100px"}}
-                    roundedCircle={true}
-                    className="fluid" 
-                    src='https://www.wrkbemanning.no/wp-content/uploads/2017/04/profile-pic-dummy.jpg' 
-                    alt="foto de perfil"
-                />
-                <h2>Pepito Martinez</h2>
-                <p>Infomarción</p>
-                
-            </ContainerNamePhoto>
+            <Container className="mt-3">
+                <UserBasicInfo>
+                    <ContainerNamePhoto>
+                        <Image 
+                            style={{width:"100px"}}
+                            roundedCircle={true}
+                            className="d-flex align-middle justify-content-center" 
+                            src='https://www.wrkbemanning.no/wp-content/uploads/2017/04/profile-pic-dummy.jpg' 
+                            alt="foto de perfil"
+                        />                                   
+                    </ContainerNamePhoto>
+                    <UserInfo>
+                        <UserName>Pepito Martinez</UserName>
+                        <p>Profeción</p>    
+                        <p>Pais / Ciudad</p>
+                    </UserInfo> 
+                </UserBasicInfo> 
+                <AboutMe>
+                    <TitleAboutMe>Acerca de mí</TitleAboutMe>
+                    <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    </AboutMeText>
+                </AboutMe>          
+                <AboutMe>
+                    <TitleAboutMe>Educación</TitleAboutMe>
+                    <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    </AboutMeText>
+                </AboutMe>   
+                <AboutMe>
+                    <TitleAboutMe>Certificados</TitleAboutMe>
+                    <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    </AboutMeText>
+                </AboutMe>        
+                <AboutMe>
+                    <TitleAboutMe>Aptitudes</TitleAboutMe>
+                    <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    </AboutMeText>
+                </AboutMe>          
+                <AboutMe>
+                    <TitleAboutMe>Intereses</TitleAboutMe>
+                    <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                    </AboutMeText>
+                </AboutMe>                    
+            </Container>            
         </>
     )
 }
--- src/components/Profile/ProfileStyled.js
+++ src/components/Profile/ProfileStyled.js
@@ -2,7 +2,40 @@ import styled from "styled-components"
 
 export const ContainerNamePhoto = styled.div`
     display: flex;
+`;
+
+export const UserName = styled.h2`
+    text-align: left;
+`;
+
+export const UserInfo = styled.div`
+    display: flex;
     flex-direction: column;
-    align-items: center;
-    margin-top: 2em;
+    align-items: left;
+    justify-content: center;
+    
+`;
+
+export const UserBasicInfo = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    padding: 1em;
+`;
+
+export const AboutMeText = styled.div`
+    margin-left: 1em;
+`;
+
+export const AboutMe = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 70vw;
+    background: #f7f7f7;
+    padding: 1.5em;
+    border-radius: 0.3em;
+    margin-top: 0.7em;
+`;
+export const TitleAboutMe = styled.h3`
+    text-align: left;
 `;
