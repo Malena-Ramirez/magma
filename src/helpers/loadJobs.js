261489ea230af32eb2163311ce160ada1cad90b0
geriosla99 <geriosla@gmail.com>
2021-08-09 19:37:39 -0500

    fix helper

--- src/helpers/loadJobs.js
+++ src/helpers/loadJobs.js
@@ -5,8 +5,14 @@ export const loadJobs = async (uid) => {
     const jobsStore = await db.collection(`${uid}/job/newJobs`).get()
     const jobs = []
 
-    console.log(jobsStore)
+    jobsStore.forEach(hijo =>{
+        jobs.push({
+            id:hijo.id,
+            ...hijo.data()
+        })
+    })
 
+    console.log(jobs)
     return jobs
 
-}
+} 
