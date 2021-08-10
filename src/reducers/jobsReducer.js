e099ae58772a4ffd36df3530a65c21720b01878d
geriosla99 <geriosla@gmail.com>
2021-08-09 19:37:59 -0500

    add more things to reducer

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -8,11 +8,23 @@ const initialState = {
 export const jobsReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.jobsAddNew:
+      return {
+        ...action.state,
+        jobsAddNew: [action.payload, ...state.jobsAddNew]
+      };
+
+        case types.jobsActive:
             return{
                 ...state, 
                 active:{
                     ...action.payload
                 }
+            }
+        case types.jobsLoad:
+            console.log(action.payload)
+            return{
+                ...state,
+                jobs:[...action.payload]
             }
         default:
             return state;
