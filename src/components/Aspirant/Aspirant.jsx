2ccc44277ca6fd6f997221a00c1acb129d7ef2d6
geriosla99 <geriosla@gmail.com>
2021-08-12 00:45:53 -0500

    Make Aspirant Component

---
+++ src/components/Aspirant/Aspirant.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { TitlePages } from '../Jobs/JobsStyled'
+import { ImgTop } from './AspirantStyled'
+
+const Aspirant = () => {
+    return (
+        <>
+        <ImgTop>
+        </ImgTop>
+        <TitlePages>
+            Aspirantes
+        </TitlePages> 
+        <Container>
+
+        </Container>
+        </>
+    )
+}
+
+export default Aspirant
