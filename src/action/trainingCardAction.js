344b6a2700cde799392513aac3a83a4c9491c3d3
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-12 12:46:20 -0500

    Create action to delete training

--- src/action/trainingCardAction.js
+++ src/action/trainingCardAction.js
@@ -1,6 +1,8 @@
+import Swal from "sweetalert2";
 import { db } from "../firebase/firebaseConfig";
 import { loadTrainingCard } from "../helpers/loadTrainingCard";
 import { types } from "../types/types";
+import { color } from "../components/GlobalStyles/color";
 
 export const activeTraining = (id, training) => {
   return {
@@ -61,9 +63,45 @@ export const startSaveTraining = (trainings) => {
   return async (dispatch) => {
     const { id, ...trainingToFirestore } = trainings;
     trainingToFirestore.updateDate = getUpdateDate();
+
     await db.doc(`/trainings/${id}`).update(trainingToFirestore);
+
     const training = await loadTrainingCard();
     dispatch(setTraining(training));
     dispatch(startLoadingAllTrainings());
+  }
+}
+
+export const deleteTraining = (id) => ({
+  type: types.trainingCardDelete,
+  payload: id
+})
+
+export const startDeleting = (id) => {
+  return async (dispatch) => {
+    await Swal.fire({
+      title: '¿Desea eliminar la capacitación?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: color.main,
+      cancelButtonColor: color.secondary,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Eliminada',
+          text: 'La capacitación ha sido eliminada exitosamente.',
+          icon: 'success',
+          confirmButtonColor: color.main,
+        }
+        );
+        db.doc(`/trainings/${id}`).delete();
+
+        dispatch(deleteTraining(id));
+      }
+    })
+
+
   }
 }
