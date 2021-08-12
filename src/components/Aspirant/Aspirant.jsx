268a1b224b640092f5928cb7fcd79a9d691d11ef
geriosla99 <geriosla@gmail.com>
2021-08-12 10:48:40 -0500

    add functions to aspirant

--- src/components/Aspirant/Aspirant.jsx
+++ src/components/Aspirant/Aspirant.jsx
@@ -1,19 +1,60 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, FloatingLabel, Form } from 'react-bootstrap'
 import { TitlePages } from '../Jobs/JobsStyled'
-import { ImgTop } from './AspirantStyled'
+import AspirantCard from './AspirantCard'
+import { ContainerInfoAspirant, ImgTop } from './AspirantStyled'
 
 const Aspirant = () => {
+
+    const aspirants = [
+        {
+            'id': 1,
+            'img': 'https://i.imgur.com/RGOwRp5.jpg',
+            'name': 'Sarah Hills'
+        },
+        {
+            'id': 2,
+            'img': 'https://i.imgur.com/5zqKoG9.png',
+            'name': 'José Manrrique'
+        },
+        {
+            'id': 3,
+            'img': 'https://i.imgur.com/gseyOXL.png',
+            'name': 'Ricardo Ramirez'
+        }
+    ]
+
     return (
         <>
         <ImgTop>
         </ImgTop>
-        <TitlePages>
-            Aspirantes
-        </TitlePages> 
         <Container>
-
+            <TitlePages>
+            Aspirantes
+            </TitlePages> 
+            <ContainerInfoAspirant>
+                <Form.Group controlId='formBasicSearch' className="mb-3">
+                    <FloatingLabel controlId='floatingSearch' label='Buscar aspirante'>
+                        <Form.Control
+                            type='search' 
+                            placeholder='Buscar'
+                            name='search'
+                        />
+                    </FloatingLabel>
+                </Form.Group>
+                {
+                    aspirants.map(aspirant =>(
+                        <AspirantCard
+                            key={aspirant.id    }
+                            img={aspirant.img}
+                            name={aspirant.name}
+                        />
+                    ))
+                }
+                
+            </ContainerInfoAspirant>
         </Container>
+        
         </>
     )
 }
