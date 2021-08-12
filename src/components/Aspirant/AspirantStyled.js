e126c346cfe136b1076ae626e433ef7676306c5a
geriosla99 <geriosla@gmail.com>
2021-08-12 00:47:52 -0500

    make styles for the component aspirant

---
+++ src/components/Aspirant/AspirantStyled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import { color } from "../GlobalStyles/color";
+
+export const ImgTop = styled.div`
+background-image: url('https://i.imgur.com/ir9dROE.jpg');
+width: 100%;
+height: 35vh;
+background-size: cover;
+background-repeat: no-repeat;
+background-position: center;
+filter: brightness(40%); // Más oscura
+margin-bottom: 2em;
+`;
