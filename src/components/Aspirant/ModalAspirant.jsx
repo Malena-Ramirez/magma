2da8e90ba81af4c5999cab78986e021e4292a26f
geriosla99 <geriosla@gmail.com>
2021-08-12 10:47:13 -0500

    create a modal

---
+++ src/components/Aspirant/ModalAspirant.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { DescriptionJobTop } from '../Jobs/JobsStyled';
+import { ProfileEdit } from '../Profile/ProfileStyled';
+
+const MyVerticallyCenteredModal = (props) => {
+
+    const history = useHistory();
+      const handleEdit = () =>{
+          history.push("/editar-perfil");
+      }
+  
+    const handleClick = () => {
+      props.onHide();
+      Swal.fire({
+        title: 'Exitos!',
+        text: 'Tu correo ha sido enviado!',
+        icon: 'success',
+        confirmButtonText: 'me encanta'
+      }); 
+      
+    }
+    
+  
+    return (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+  
+          <Modal.Header closeButton>            
+                <Modal.Title id="contained-modal-title-vcenter">
+                  <h4 className=''>Nombre de la profesión</h4>
+                  <DescriptionJobTop>Fecha de nacimiento | Lugar donde vive} </DescriptionJobTop> 
+                </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className='row'>          
+                <div className='col-3'>
+                  <img src='' 
+                    alt='Nombre de la empresa' 
+                    style={{width: 100}}
+                  />
+                  <DescriptionJobTop className="m-3">Conocimientos extras</DescriptionJobTop>
+                  <DescriptionJobTop className="m-3">Idiomas</DescriptionJobTop>
+                  <DescriptionJobTop className="m-3">Informacion de contacto</DescriptionJobTop>
+                </div>              
+                    <div className='col-9'>
+                      <h4>Descripción</h4>
+                      <p>
+                        Sobre mí
+                      </p>
+                      <h4>Requisitos</h4>
+                      <p>
+                        Certificados, intereses,              
+                      </p>                    
+                    </div>
+  
+            <ProfileEdit
+              onClick={handleEdit}
+            >
+              <p>Editar Empleo</p>
+            </ProfileEdit>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleClick} variant="outline-warning">Contacto</Button>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+    );
+}
+
+  export default MyVerticallyCenteredModal;
