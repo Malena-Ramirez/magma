d977d28a182e5ddc2c9b779c2a73355570d69886
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-15 19:29:55 -0500

    Create edit and delete action

--- src/action/jobsAction.js
+++ src/action/jobsAction.js
@@ -1,6 +1,8 @@
 import { db } from "../firebase/firebaseConfig";
 import { types } from "../types/types";
 import { loadJobs } from "../helpers/loadJobs";
+import Swal from "sweetalert2";
+import { color } from "../components/GlobalStyles/color";
 
 export const activeJob = (id, jobs) => {
     return {
@@ -48,5 +50,50 @@ export const startLoadingJobs = () => {
     return async (dispatch) => {
         const jobs = await loadJobs()
         dispatch(setJobs(jobs))
+    }
+}
+
+export const startSaveJobs = (job) => {
+    return async (dispatch) => {
+        const { id, ...jobToFirestore } = job;
+        jobToFirestore.updateDate = getUpdateDate();
+
+        await db.doc(`/Job/${id}`).update(jobToFirestore);
+
+        const jobs = await loadJobs();
+        dispatch(setJobs(jobs));
+        dispatch(startLoadingJobs());
+    }
+}
+
+export const deleteJob = (id) => ({
+    type: types.jobsDelete,
+    payload: id
+})
+
+export const startDeletingJob = (id) => {
+    return async (dispatch) => {
+        await Swal.fire({
+            title: '¿Desea eliminar la vacante?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: color.main,
+            cancelButtonColor: color.secondary,
+            confirmButtonText: 'Aceptar',
+            cancelButtonText: 'Cancelar',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Eliminada',
+                    text: 'La vacante ha sido eliminada exitosamente.',
+                    icon: 'success',
+                    confirmButtonColor: color.main,
+                }
+                );
+                db.doc(`/Job/${id}`).delete();
+
+                dispatch(deleteJob(id));
+            }
+        })
     }
 }
