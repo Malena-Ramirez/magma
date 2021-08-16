ba7863b8fc471a29f33f10409b228678fd236f8b
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-16 12:47:17 -0500

    Combine reducers

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import { jobsReducer } from "../reducers/jobsReducer.js";
 import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { profileReducer } from "../reducers/profileReducer.js";
+import { userReducer } from "../reducers/userReducer.js";
 
 const composeEnhancers = (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
@@ -13,7 +14,8 @@ const reducers = combineReducers({
     login: loginReducer,
     trainingCard: trainingCardReducer,
     jobs: jobsReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    companyUser: userReducer,
 })
 
 export const store = createStore(
