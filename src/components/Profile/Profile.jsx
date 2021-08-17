4780c5b77b589a0516b6fa35c302d76b12bf7d3e
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-17 18:03:48 -0500

    Add edit profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,110 +1,91 @@
-import React from 'react'
-import { ContainerNamePhoto, 
-    UserName, 
-    UserInfo, 
-    UserBasicInfo, 
-    AboutMeText, 
-    AboutMe, 
-    TitleAboutMe,
-    PublicityBusiness,
-    MoreInfo,
-    Sponsor, 
-    ProfileEdit} from "./ProfileStyled";
-import { Image, Container } from 'react-bootstrap'
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+  ContainerNamePhoto,
+  UserName,
+  UserInfo,
+  UserBasicInfo,
+  AboutMeText,
+  AboutMe,
+  TitleAboutMe,
+  MoreInfo,
+  ProfileEdit,
+  ProfileContainer,
+} from './ProfileStyled';
+import { Image } from 'react-bootstrap';
+import FormProfile from './Form/FormProfile';
 
 const Profile = () => {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleClick = () => {
+    setShowEdit(true);
+  };
 
-    const history = useHistory();
-    const handleClick = () =>{
-        history.push("/editar-perfil");
-    }
+  return (
+    <>
+      <ProfileContainer>
+        {showEdit ? (
+          <FormProfile setShowEdit={setShowEdit} />
+        ) : (
+          <>
+            <UserBasicInfo>
+              <ContainerNamePhoto>
+                <Image
+                  style={{ width: '100px', height: '100px' }}
+                  roundedCircle={true}
+                  className='d-flex align-middle justify-content-center'
+                  src='https://www.wrkbemanning.no/wp-content/uploads/2017/04/profile-pic-dummy.jpg'
+                  alt='foto de perfil'
+                />
+              </ContainerNamePhoto>
+              <UserInfo>
+                <UserName>Juan José Campos</UserName>
+                <span>Contador público</span>
+                <span>Medellín, Colombia</span>
+              </UserInfo>
+              <ProfileEdit
+                className='bi bi-pencil-square fs-5'
+                onClick={handleClick}
+              ></ProfileEdit>
+            </UserBasicInfo>
 
-    return (
-        <>
-            <Container className="mt-3">
-                <UserBasicInfo>
-                    <ContainerNamePhoto>
-                        <Image 
-                            style={{width:"100px", height: "100px"}}
-                            roundedCircle={true}
-                            className="d-flex align-middle justify-content-center" 
-                            src='https://www.wrkbemanning.no/wp-content/uploads/2017/04/profile-pic-dummy.jpg' 
-                            alt="foto de perfil"
-                        />                                   
-                    </ContainerNamePhoto>
-                    <UserInfo>
-                        <UserName>Pepito Martinez</UserName>
-                        <p>Profesión</p>    
-                        <p>País / Ciudad</p>
-                    </UserInfo> 
-                    <ProfileEdit
-                        onClick={handleClick}
-                    >
-                        <p>Editar Perfil</p>
-                    </ProfileEdit>
-                </UserBasicInfo>                
-                <MoreInfo>
+            <MoreInfo>
+              <div>
+                <AboutMe>
+                  <TitleAboutMe>Información de contacto</TitleAboutMe>
+                  <AboutMeText>
                     <div>
-                        <AboutMe>
-                            <TitleAboutMe>Acerca de mí</TitleAboutMe>
-                            <AboutMeText>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
-                            </AboutMeText>
-                        </AboutMe>          
-                        <AboutMe>
-                            <TitleAboutMe>Educación</TitleAboutMe>
-                            <AboutMeText>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
-                            </AboutMeText>
-                        </AboutMe>   
-                        <AboutMe>
-                            <TitleAboutMe>Certificados</TitleAboutMe>
-                            <AboutMeText>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
-                            </AboutMeText>
-                        </AboutMe>        
-                        <AboutMe>
-                            <TitleAboutMe>Aptitudes</TitleAboutMe>
-                            <AboutMeText>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
-                            </AboutMeText>
-                        </AboutMe>          
-                        <AboutMe>
-                            <TitleAboutMe>Intereses</TitleAboutMe>
-                            <AboutMeText>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
-                            </AboutMeText>
-                        </AboutMe>                                  
-                    </div>  
-                    <PublicityBusiness>
-                        <Sponsor>
-                            <h5>Pueba nuestras capacitaciones!!!</h5>
-                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.</p>
-                        </Sponsor>
-                        <Sponsor>
-                            <h5>Mira los nuevos empleos que hay para tí!!!</h5>
-                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.</p>
-                        </Sponsor>
-                    </PublicityBusiness>              
-                </MoreInfo>                                               
-            </Container>            
-        </>
-    )
-}
+                      <i className='bi bi-telephone-fill me-2'></i>
+                      <span>1234567890</span>
+                    </div>
+                    <div>
+                      <i className='bi bi-envelope-fill me-2'></i>
+                      <span>correo@correo.com</span>
+                    </div>
+                  </AboutMeText>
+                </AboutMe>
+                <AboutMe>
+                  <TitleAboutMe>Acerca de mí</TitleAboutMe>
+                  <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo
+                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                    risus, porta ac consectetur ac, vestibulum at eros.
+                  </AboutMeText>
+                </AboutMe>
+                <AboutMe>
+                  <TitleAboutMe>Educación</TitleAboutMe>
+                  <AboutMeText>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo
+                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                    risus, porta ac consectetur ac, vestibulum at eros.
+                  </AboutMeText>
+                </AboutMe>
+              </div>
+            </MoreInfo>
+          </>
+        )}
+      </ProfileContainer>
+    </>
+  );
+};
 
 export default Profile;
