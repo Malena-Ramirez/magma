05521dbfcd49beba8417fad7547db32838fd424e
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-17 18:07:29 -0500

    Set state in form profile

--- src/components/Profile/Form/FormProfile.jsx
+++ src/components/Profile/Form/FormProfile.jsx
@@ -1,114 +1,94 @@
-import React from 'react'
-import { Form, FloatingLabel, Container, Button } from 'react-bootstrap';
+import React from 'react';
+import { Form, FloatingLabel, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-import { profileAction } from '../../../action/profileAction';
+import { color } from '../../GlobalStyles/color';
+// import { profileAction } from '../../../action/profileAction';
 import { TitlePages } from '../../Jobs/JobsStyled';
-import { ImgTop } from '../ProfileStyled';
 
-const FormProfile = () => {
+const FormProfile = ({ setShowEdit }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const handleCancel = () => {
+    setShowEdit(false);
+  };
 
-    return (
-        <>            
-            <ImgTop></ImgTop>
-            <Container className="my-3">
-                <TitlePages>Edita tu perfil</TitlePages>
-            <Form className="my-3">
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Profesión"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Profesión"
-                            name='profession'
-                            // value={profession} 
-                            // onChange={handleInputChange}
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Ciudad"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Ciudad" 
-                            name='city'
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Acerca de mí"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Acerca de mí" 
-                            name='aboutMe'
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Educación"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Educación" 
-                            name='education'
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Certificados"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Certificados" 
-                            name='certificates'
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Aptitudes"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Aptitudes"
-                            name='skills' 
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingInput" 
-                        label="Intereses"
-                        className="mb-3"
-                    >
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Intereses"
-                            name='interest' 
-                        />
-                    </FloatingLabel>
-                    <div className="d-grid gap-2">
-                            <Button 
-                                variant="warning" 
-                                size="lg" 
-                                className="mb-3"
-                                value="submit"
-                                onClick={()=> dispatch(profileAction())}
-                            >
-                                Enviar
-                            </Button>            
-                        </div>
-                </Form>
-            </Container>                
-        </>
-    )
-}
+  return (
+    <>
+      <TitlePages>Editar perfil</TitlePages>
+      <Form className='my-3'>
+        <FloatingLabel
+          controlId='floatingInput'
+          label='Teléfono'
+          className='mb-3'
+        >
+          <Form.Control
+            type='number'
+            placeholder='Teléfono'
+            name='phoneNumber'
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId='floatingInput'
+          label='Profesión'
+          className='mb-3'
+        >
+          <Form.Control
+            type='text'
+            placeholder='Profesión'
+            name='profession'
+            // value={profession}
+            // onChange={handleInputChange}
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId='floatingInput'
+          label='Ciudad'
+          className='mb-3'
+        >
+          <Form.Control type='text' placeholder='Ciudad' name='city' />
+        </FloatingLabel>
 
-export default FormProfile
+        <FloatingLabel
+          controlId='floatingInput'
+          label='Educación'
+          className='mb-3'
+        >
+          <Form.Control type='text' placeholder='Educación' name='education' />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId='floatingInput'
+          label='Acerca de mí'
+          className='mb-3'
+        >
+          <Form.Control
+            as='textarea'
+            style={{ height: '150px' }}
+            placeholder='Acerca de mí'
+            name='aboutMe'
+          />
+        </FloatingLabel>
+        <div className='d-flex justify-content-center'>
+          <Button
+            variant='warning'
+            size='lg'
+            className='mb-3'
+            style={{ width: '150px' }}
+            type='submit'
+          >
+            Enviar
+          </Button>
+          <Button
+            size='lg'
+            className='mb-3 ms-3'
+            style={{ width: '150px', backgroundColor: color.main }}
+            onClick={handleCancel}
+          >
+            Cancelar
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default FormProfile;
