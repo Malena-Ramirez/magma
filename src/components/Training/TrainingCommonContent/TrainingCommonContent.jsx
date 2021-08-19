4defc8c17d13e2571eea9ea0e3a18870d91f46a0
geriosla99 <geriosla@gmail.com>
2021-08-18 22:50:27 -0500

    create function category

--- src/components/Training/TrainingCommonContent/TrainingCommonContent.jsx
+++ src/components/Training/TrainingCommonContent/TrainingCommonContent.jsx
@@ -2,24 +2,24 @@ import React, { useEffect, useState, useMemo } from 'react';
 import { Accordion, FloatingLabel, Form, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import TrainingCards from '../TrainingCards';
-import { AsideBar, MainContent, NoTrainingContainer, NoTrainingImg, TrainingContent } from '../TrainingStyled';
+import { AsideBar, MainContent, TrainingContent } from '../TrainingStyled';
 
 const TrainingCommonContent = () => {
   
   const { trainingCard } = useSelector((state) => state.trainingCard);
   //funciones de busqueda
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('')
+  // const [search, setSearch] = useState('')
 
   useEffect(() => {
     setData(trainingCard);
   }, [trainingCard]);
-  const handleSearch = (evento) => {
-    setSearch(evento.target.value);
-    };
-  const dataFiltered= useMemo(() => data.filter((item) => {
-    return item.title.toLowerCase().includes(search.toLowerCase());
-  }), [data, search]);
+  // const handleSearch = (evento) => {
+  //   setSearch(evento.target.value);
+  //   };
+  // const dataFiltered= useMemo(() => data.filter((item) => {
+  //   return item.title.toLowerCase().includes(search.toLowerCase());
+  // }), [data, search]);
 
   //funciones de categoria
   const categories = [
@@ -55,37 +55,16 @@ const TrainingCommonContent = () => {
     },
   ]
   const [checkbox, setCheckbox] = useState('');
-  const [resetState, setResetState] = useState([])
-  const [showCard, setShowCard] = useState(false)
-  useEffect(() => {
-    const matchCategories = data.filter( (item) => item.category === checkbox);    
-    setResetState(matchCategories);
-  }, [data, checkbox])
-
-  console.log(resetState);
 
   const handleChange = (e) => {
     setCheckbox(e.target.value)
   }
 
-  console.log(checkbox);
-
   const dataCategory= useMemo(() => data.filter((item) => {
     return item.category.toLowerCase().includes(checkbox.toLowerCase());
   }), [data, checkbox]);
 
-  console.log(dataCategory);
-  const checked = () => {
-    if('checked' && resetState.length>0){
-      const objectAdd = () => {
-        for (let i = 0; i < resetState.length; i++) {
-          dataCategory.push(resetState.includes(resetState>0));
-        }
-        
-      }
-    }
-  }
-  console.log(checked())
+
   return (
     <TrainingContent>
       <AsideBar>
@@ -98,7 +77,7 @@ const TrainingCommonContent = () => {
             type='text'
             placeholder='Buscar capacitaciones'
             name='term'
-            onChange={handleSearch}
+            // onChange={handleSearch}
           />
         </FloatingLabel>
         <Accordion defaultActiveKey='0'>
@@ -116,7 +95,6 @@ const TrainingCommonContent = () => {
                   label={cat.name}
                   value={cat.category}
                   onChange={handleChange}  
-                  // onClick={checked}
                 /> 
               ))}            
               </Form>
@@ -127,7 +105,7 @@ const TrainingCommonContent = () => {
       <MainContent>
         <Row xs={1} md={2} lg={3} className='g-4'>
           {data && 
-             dataFiltered.map((training, index) => (
+             dataCategory.map((training, index) => (
               <TrainingCards key={index} training={training} company={false} />
           ))}
         </Row>
